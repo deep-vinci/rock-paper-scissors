5a96b8623160e5bf07fdaca3c55129226f6c8810
Deepak <deepakworkpc@gmail.com>
2024-06-01 23:30:37 +0530

    adding getComputerChoice()

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("hello world!")
+
+const options = ["rock", "paper", "scissors"]
+
+function getComputerChoice() {
+    console.log(
+        options[Number(String(Math.random()).at(-1))%3]
+    )
+}
+getComputerChoice();
