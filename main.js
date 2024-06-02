d5ed9211b710dce306e25d28727c2912456a507e
Deepak <deepakworkpc@gmail.com>
2024-06-03 00:18:50 +0530

    cleanup of useless code

--- main.js
+++ main.js
@@ -8,18 +8,6 @@ function getComputerChoice() {
     return options[cmputerChoice];
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt(`choose your options
-    0. Rock [0]
-    1. Paper [1]
-    2. Scissors [2]
-    
-    3. restart game [r]
-    4. end game [q]`);
-
-    return humanChoice;
-}
-
 let combinations = {
     rock_wins : ['rock', 'scissors'],
     scissors_wins : ['scissors', 'paper'],
@@ -67,35 +55,6 @@ function playRound([...choices]) {
     }
 }
 
-
-function playGame() {
-    
-    while (true) {
-        let humanSelection = getHumanChoice();
-        if (humanSelection == "q") {
-            if (humanScore === computerScore) {
-                console.log("%c its a tie!!", "background-color:yellow, color:black")
-            } else if (humanScore > computerScore) {
-                console.log("%c human won!!", "background-color:green")
-            } else {
-                console.log("%c computer won!!", "background-color:red")
-            }        
-            console.log(`human score: ${humanScore}, compuer score: ${computerScore}`)
-            break;
-        } else if (humanSelection == "r") {
-            humanScore = 0; computerScore = 0;
-            playGame();
-            break;
-        } else {
-            let computerSelection = getComputerChoice();
-            console.log(`%c human : ${options[humanSelection]}`, "background-color:black");
-            playRound([options[humanSelection], computerSelection]);    
-        }
-    }
-}   
-
-// playGame();
-
 const keyPressOptions = {
     r: "rock",
     p: "paper",
@@ -103,22 +62,10 @@ const keyPressOptions = {
 };
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    // const loader = document.getElementById('loader');
-    // const dots = ['.', '..', '...'];
-    // let currentIndex = 0;
-
-    // setInterval(() => {
-    //     loader.textContent = dots[currentIndex];
-    //     currentIndex = (currentIndex + 1) % dots.length;
-    // }, 500);
-
     document.addEventListener('keydown', event => {
         if (keyPressOptions.hasOwnProperty(event.key)) {
             let computerSelection = getComputerChoice();
-            // alert(`
-            // ${computerSelection} - computer
-            // ${keyPressOptions[event.key]} - human
-            // `)
+
             document.querySelector(".computerchoice").textContent = computerSelection;
             document.querySelector(".userchoice").textContent = keyPressOptions[event.key];
             
@@ -146,7 +93,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
             scorePage.appendChild(eachScorePage);
             eachScorePage.appendChild(playerChoices);
             eachScorePage.appendChild(result);
-            // alert(`${humanScore}, ${computerScore}`)
         }
     });
 
@@ -163,7 +109,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         eachScorePage.forEach((i, e) => {
             scorePage.removeChild(i)
         })
-        
 
     })
 
