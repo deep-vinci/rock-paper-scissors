4f5470fdc4114d7871c6fa1194a55dbbd20aff9d
Deepak <deepakworkpc@gmail.com>
2024-06-09 01:33:37 +0530

    some logic comment for future me

--- main.js
+++ main.js
@@ -13,6 +13,17 @@ let buttons = document.querySelectorAll(".choice-btn");
 
 buttons.forEach( button => {
     button.addEventListener("click", () => {
-        console.log(button);
+
+        // - on click event fires up, determines who is the clicker <user>, 
+        //   waits for the other user to click, then same
+
+        // ------
+
+        // - call the user-0 and ask for its choice
+        // - user-0 when choosen the choice, sends a event update, that is listened 
+        //   by a function whoWon() that determines the winner
+        // - whoWon sends back the data as object, { winner: <user>, choices: [rock, paper]}
+        // - this data is used to increment the counter for <user> wins
+        // - after the required round numbers, the winner is declared and status message is popped, You win!
     })
 })
